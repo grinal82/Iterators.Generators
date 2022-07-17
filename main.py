c10a110f17b2a505ed43004022ee03be9311cf70
girnal82 <grinal82@gmail.com>
2022-07-17 20:53:18 +0300

    added generator working with multiple layers of nested lists

--- main.py
+++ main.py
@@ -23,30 +23,65 @@ class Flat_iterator:
         if self.start == self.end:
             raise StopIteration
         return self
-    # reassigning "__str__" method to print out the result of itteration el by el
 
+    # reassigning "__str__" method to print out the result of itteration el by el
     def __str__(self):
         return '\n'.join(str(el)for el in nested_list[self.start])
 
+    # method to print out the "unpacked" nested list as a single list of all elements
     def chain_list(nested_list):
         print(list(chain.from_iterable(nested_list)))
 
 
+# Написать генератор, который принимает список списков, и возвращает их плоское представление
 class Flat_generator:
 
     def flat_generator(self, nested_list):
         return [num for el in nested_list for num in el]
 
 
+# Дополнительное задание: *аписать генератор, обрабатывающий списки с любым уровнем вложенности
+nested_list2 = [
+    ['a', 'b', 'c'],
+    ['d', 'e', 'f', 'h', False],
+    [1, 2, None, [1, 2, 3, 4, 5, [True, None, False]]],
+]
+
+
+def flat_generator(nested_list2):
+    for el in chain(nested_list2):
+        if isinstance(el, list):
+            yield from flat_generator(el)
+        else:
+            yield el
+
+
 if __name__ == '__main__':
     # printing "unpacked" nested list element by element
+    print('Печатаем каждый элемент вложенного списка списков')
     for elem in Flat_iterator(nested_list):
         print(elem)
+
     print("--------------------------------------------")
-    # printing "unpacked" nested list in a single list
+
+    # printing "unpacked" nested list in a single list of all elements
+    print('Печатаем элементы вложенного списка списков в один список')
     Flat_iterator.chain_list(nested_list)
+
     print('--------------------------------------------')
+
     # printing generated elements from nested list one by one
+    print('Генератор элементоы вложенного списка списков')
     result = Flat_generator()
     for el in result.flat_generator(nested_list):
         print(el)
+
+    print('------------------------------------------------')
+
+    # printing generated elements from multiple-layered nested list
+    print('Генератоп элементов из списка списков любого уровня вложенности')
+    flat_list_3 = [item for item in flat_generator(nested_list2)]
+    for el in flat_list_3:
+        print(el)
+
+    print()
